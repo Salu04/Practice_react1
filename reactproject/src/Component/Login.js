67020a1ee83e40ba447e06ec0ea70a655bb1b61b
Saloni Patil <saloni.patil@pattern.com>
2023-05-24 16:39:41 +0530

    merge changes

--- reactproject/src/Component/Login.js
+++ reactproject/src/Component/Login.js
@@ -7,6 +7,7 @@ const Login = () => {
   return (
     <div>
         <h1>login component</h1>
+        <p>Plaease logine here</p>
       <button onClick={()=>changeStatus(true)}>login</button>
     </div>
   )
--- reactproject/src/Component/Product.js
+++ reactproject/src/Component/Product.js
@@ -7,6 +7,8 @@ export const Product = () => {
       <h1>Product Component</h1>
       <p>Please search product </p>
       <p>Hi I am saloni in learning phase now ia m going to chnage this</p>
+      <h5>i here i have chnages in to loaran branch</h5>
+      <p>hello checking merging phase</p>
     </div>
   )
 }
